48b597fd3274828026488b5141c00c9fc2f94a10
shimaamo7amed <shimaa0mohamed19@gmail.com>
2024-07-24 13:37:23 +0300

    edit

--- app/Services/users/EventsEventsServices.php
+++ app/Services/users/EventsEventsServices.php
@@ -20,12 +20,12 @@ class EventsEventsServices
     {
         if ($language == 'ar') {
             $event = EventsEventsM::with(['speakers' => function ($query) {
-                $query->select('id', 'event_id', 'name_ar as name', 'title_ar as title', 'shortDescription_ar as description','longDescription_ar as description', 'image');
+                $query->select('id', 'event_id', 'name_ar as name', 'title_ar as title', 'shortDescription_ar as description','longDescription_ar as description', 'image', 'facebook', 'twitter', 'website');
             }])->select('id', 'title_ar as title', 'long_desc_ar as description', 'image','price')->find($id);
             return $event;
         } else {
             $event = EventsEventsM::with(['speakers' => function ($query) {
-                $query->select('id', 'event_id', 'name_en as name', 'title_en as title', 'shortDescription_en as description','longDescription_en as description', 'image');
+                $query->select('id', 'event_id', 'name_en as name', 'title_en as title', 'shortDescription_en as description','longDescription_en as description', 'image', 'facebook', 'twitter', 'website');
             }])->select('id', 'title_en as title', 'long_desc_en as description', 'image','price')->find($id);
             return $event;
 
