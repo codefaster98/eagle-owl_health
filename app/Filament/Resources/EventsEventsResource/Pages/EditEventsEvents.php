33dd5907cc2878d045db2062cf7659f1f1458f3a
shimaamo7amed <shimaa0mohamed19@gmail.com>
2024-08-13 13:08:33 +0300

    ..

--- app/Filament/Resources/EventsEventsResource/Pages/EditEventsEvents.php
+++ app/Filament/Resources/EventsEventsResource/Pages/EditEventsEvents.php
@@ -34,32 +34,6 @@ class EditEventsEvents extends EditRecord
         return $data;
     }
 
-    // public function form(Form $form): Form
-    // {
-    //     return $form
-    //         ->schema([
-    //             TextInput::make('title_ar')->required()->label("Arabic Title"),
-    //             TextInput::make('title_en')->required()->label("English Title"),
-    //             TextInput::make('price')->numeric()->prefix('€')->maxValue(42949672.95)->required()->label("Price"),
-    //             TextInput::make('date')->required()->label("Date"),
-    //             TextInput::make('date_details')->required()->label("Date Details"),
-    //             TextInput::make('from_time')->required()->label("From_Time"),
-    //             TextInput::make('to_time')->required()->label("To_Time"),
-    //             TextInput::make('location')->required()->label("location"),
-    //             Textarea::make('short_desc_ar')->required()->label("Arabic Short Details"),
-    //             Textarea::make('short_desc_en')->required()->label("English Short Details"),
-    //             Textarea::make('long_desc_ar')->required()->label("Arabic Long Details"),
-    //             Textarea::make('long_desc_en')->required()->label("English Long Details"),
-    //             FileUpload::make('image')->label("image")->disk('public')->directory('events_events')->visibility('public') ->required(false)
-
-    //         ])
-    //            ->Select::make('speakers')
-    //             ->label('Speakers')
-    //             ->options(SpeakersSpeakersM::all()->pluck('code', 'id'))
-    //             ->searchable()
-    //             ->multiple()
-    //         ]);
-    // }
     public function form(Form $form): Form
     {
         return $form
@@ -69,19 +43,15 @@ class EditEventsEvents extends EditRecord
                 TextInput::make('price')->numeric()->prefix('€')->maxValue(42949672.95)->required()->label("Price"),
                 TextInput::make('date')->required()->label("Date"),
                 TextInput::make('date_details')->required()->label("Date Details"),
-                TextInput::make('from_time')->required()->label("From Time"),
-                TextInput::make('to_time')->required()->label("To Time"),
-                TextInput::make('location')->required()->label("Location"),
+                TextInput::make('from_time')->required()->label("From_Time"),
+                TextInput::make('to_time')->required()->label("To_Time"),
+                TextInput::make('location')->required()->label("location"),
                 Textarea::make('short_desc_ar')->required()->label("Arabic Short Details"),
                 Textarea::make('short_desc_en')->required()->label("English Short Details"),
                 Textarea::make('long_desc_ar')->required()->label("Arabic Long Details"),
                 Textarea::make('long_desc_en')->required()->label("English Long Details"),
-                FileUpload::make('image')
-                    ->label("Image")
-                    ->disk('public')
-                    ->directory('events_events')
-                    ->visibility('public')
-                    ->required(false),
+                FileUpload::make('image')->label("image")->disk('public')->directory('events_events')->visibility('public')->required(false)
+
             ])
             ->schema([
                 Select::make('speakers')
@@ -91,6 +61,37 @@ class EditEventsEvents extends EditRecord
                     ->multiple()
             ]);
     }
+    // public function form(Form $form): Form
+    // {
+    //     return $form
+    //         ->schema([
+    //             TextInput::make('title_ar')->required()->label("Arabic Title"),
+    //             TextInput::make('title_en')->required()->label("English Title"),
+    //             TextInput::make('price')->numeric()->prefix('€')->maxValue(42949672.95)->required()->label("Price"),
+    //             TextInput::make('date')->required()->label("Date"),
+    //             TextInput::make('date_details')->required()->label("Date Details"),
+    //             TextInput::make('from_time')->required()->label("From Time"),
+    //             TextInput::make('to_time')->required()->label("To Time"),
+    //             TextInput::make('location')->required()->label("Location"),
+    //             Textarea::make('short_desc_ar')->required()->label("Arabic Short Details"),
+    //             Textarea::make('short_desc_en')->required()->label("English Short Details"),
+    //             Textarea::make('long_desc_ar')->required()->label("Arabic Long Details"),
+    //             Textarea::make('long_desc_en')->required()->label("English Long Details"),
+    //             FileUpload::make('image')
+    //                 ->label("Image")
+    //                 ->disk('public')
+    //                 ->directory('events_events')
+    //                 ->visibility('public')
+    //                 ->required(false),
+    //         ])
+    //         ->schema([
+    //             Select::make('speakers')
+    //                 ->label('Speakers')
+    //                 ->options(SpeakersSpeakersM::all()->pluck('code', 'id'))
+    //                 ->searchable()
+    //                 ->multiple()
+    //         ]);
+    // }
     protected function handleRecordUpdate(Model $record, array $data): Model
     {
 
